8793a3cbfaa82f852e93bf97b6dddcec92b1d0c0
Jasmine Kent <jk@codeartemis.com>
2016-02-27 17:17:53 +0100

    fix button layout

--- etherisk/Etherisk/Assets/Lobby/LobbyPanel.cs
+++ etherisk/Etherisk/Assets/Lobby/LobbyPanel.cs
@@ -8,7 +8,7 @@ public class LobbyPanel : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-    var position = new Vector3(0, 0, 0);
+    var position = new Vector3(0, -10, 0);
     for (int i = 0; i < 5; ++i) {
 	    GameObject button = (GameObject) Instantiate(buttonTemplate, position, Quaternion.identity);
       button.transform.SetParent(transform, false);
