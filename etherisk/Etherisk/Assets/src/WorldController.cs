7e021b19b2819c1d59d9fb63a8c9a6961104ede9
2075 <marco@lab75.jp>
2016-02-27 16:59:53 +0100

    updates...merges...

--- etherisk/Etherisk/Assets/src/WorldController.cs
+++ etherisk/Etherisk/Assets/src/WorldController.cs
@@ -1,14 +1,47 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+//public struct Player {
+//	
+//	public string name = "Player";
+//	public string address = "00000000000000000000000000000000";
+//
+//}
+//public struct Unit {
+//
+//	public int attack = 0;
+//	public int defense = 0;
+//
+//}
+//
+//public struct Country {
+//
+//	public int id = "0";
+//	public string name = "Country";
+//	public Player owner;
+//
+//}
+
 public class WorldController : MonoBehaviour {
 
-	// Use this for initialization
+	// player address array
+	// qty of players
+
+	public GameObject[] countries;
+
+	public Material freeCountryMaterial;
+	public Material ownedCountryMaterial;
+
 	void Start () {
-	
+
+		countries = GameObject.FindGameObjectsWithTag("Country");
+
+		foreach ( GameObject country in countries ) {
+			country.GetComponent<Renderer>().material = freeCountryMaterial;
+		}
+				
 	}
-	
-	// Update is called once per frame
+
 	void Update () {
 	
 	}
