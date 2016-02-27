d1d42249e31e07963d246a7329257439bbbb09e4
Jasmine Kent <jk@codeartemis.com>
2016-02-27 17:11:28 +0100

    add lobby

---
+++ etherisk/Etherisk/Assets/Lobby/LobbyPanel.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class LobbyPanel : MonoBehaviour {
+
+  public GameObject buttonTemplate;
+
+	// Use this for initialization
+	void Start () {
+    var position = new Vector3(0, 0, 0);
+    for (int i = 0; i < 5; ++i) {
+	    GameObject button = (GameObject) Instantiate(buttonTemplate, position, Quaternion.identity);
+      button.transform.SetParent(transform, false);
+      position.y -= 35;
+    }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
